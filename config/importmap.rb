b3d10a70587ac5cd704bc419ff55cf04f29aff1e
rankitranjan <rankit4u@gmail.com>
2025-02-28 12:58:21 +0530

    fix style

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,5 +3,5 @@
 pin "application"
 pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.esm.min.js"
 pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/esm/index.js"
-pin "flash"
-pin "keywords"
+pin "flash", to: "flash.js"
+pin "keywords", to: "keywords.js"
